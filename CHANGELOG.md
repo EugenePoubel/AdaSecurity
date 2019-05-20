52faad2444a965916087f498ba4ba8fe82899424
EugenePoubel <adripons123@gmail.com>
2019-05-20 20:39:06 +0200

    V1.5
    Voir Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,10 @@ V 1.0.0
 V 1.0.1
 - Suppréssion des fichiers inutile liée a la template
 - Restructuration des dossiers
+- 
+
+------
+
+V 1.5.0
+
+- Version Final du programme Arduino
--- programme arduino/programme_Final/programme_Final.ino
+++ programme arduino/programme_Final/programme_Final.ino
@@ -1,42 +1,43 @@
-// Ces deux bibliothèques sont indispensables pour le shield
+/************************Déclaration des Librairies************************************/
 #include <SPI.h>
 #include <Ethernet.h>
 #include <DHT.h>
-
-// L'adresse MAC du shield
-byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xA5, 0x7E };
-// L'adresse IP que prendra le shield
-IPAddress ip(192,168,0,143);
-// L'objet qui nous servira a la communication
-EthernetClient client;
-// Le serveur à interroger
-char serveur[] = "adasecurity.fr";
-
-// pour lire les caractères
-char carlu = 0;
-// moment de la dernière requête
-long derniereRequete = 0;
-// temps minimum entre deux requêtes
-const long updateInterval = 30000;
-// mémorise l'état de la connexion entre deux tours de loop
-bool etaitConnecte = false;
-//DHT22
-int t = 0;  // TEMPERATURE VAR
-int h = 0;  // HUMIDITY VAR
-int c = -400;
-#define DHTPIN 2 // SENSOR PIN
-#define DHTTYPE DHT22 // SENSOR TYPE - THE ADAFRUIT LIBRARY OFFERS SUPPORT FOR MORE MODELS
+/************************Déclaration de broches************************************/
+#define MG_PIN                       (A5)  
+#define DHTPIN 2
+/************************Déclaration Relative au programme des capteurs ************************************/
+#define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
+#define         READ_SAMPLE_TIMES            (10)
+#define         READ_SAMPLE_INTERVAL         (50)       
+#define         DC_GAIN                      (8.5)   // definir l'amplificater du capteur
+#define         ZERO_POINT_X                 (2.602) // definir la valeurs minimum en volt qui est capter par le capteur
+#define         ZERO_POINT_VOLTAGE           (0.324) // definir la valeur en volt quand la concentration est à 400 ppm
+#define         MAX_POINT_VOLTAGE            (0.265) // definir la valeur en volt quand la concentration est à 10000 ppm
+#define         REACTION_VOLTGAE             (0.059)  
+float CO2Curve[3]  =  {ZERO_POINT_X, ZERO_POINT_VOLTAGE, (REACTION_VOLTGAE / (2.602 - 4))};
+/************************Paramètre Réseau************************************/
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0E, 0xA5, 0x7E };
+IPAddress ip(192,168,0,143);
+EthernetClient client;
+char serveur[] = "adasecurity.fr";
+/************************Variable Relative au programme réseau************************************/
+char carlu = 0; // pour lire les caractères
+long derniereRequete = 0; // moment de la dernière requête
+const long updateInterval = 30000; // temps minimum entre deux requêtes
+bool etaitConnecte = false; // mémorise l'état de la connexion entre deux tours de loop
+char erreur = 0;
+int t = 0;  
+int h = 0;  
+int c = -1;
 String data;
+/***********************************************************************************************/
 void setup() {
   // On démarre la voie série pour déboguer
   Serial.begin(9600);
   dht.begin(); 
-
-  char erreur = 0;
-  // On démarre le shield Ethernet SANS adresse IP (donc donnée via DHCP)
   erreur = Ethernet.begin(mac);
-
+/************************attribution IP Automatique (DHCP)************************************/
   if (erreur == 0) {
     Serial.println("Parametrage avec ip fixe...");
     // si une erreur a eu lieu cela signifie que l'attribution DHCP
@@ -48,9 +49,18 @@ void setup() {
   delay(1000);
   Serial.println("Pret !");
 }
-
+/***********************************************************************************************/
 void loop()
 {
+int percentage;                                       // declarer la variable dans lequelle on mettra la valeur en ppm
+float volts;                                          // declarer la variable dans lequelle on mettra la valeur en volt
+volts = MGRead(MG_PIN);  
+percentage = MGGetPercentage(volts, CO2Curve); 
+/**********************Récupération des Valeurs des Capteurs**********************************/
+h = (int) dht.readHumidity(); 
+t = (int) dht.readTemperature();
+c = percentage;
+data = String("temp1=") + t + "&hum1=" + h + "&co21=" + c;
   // on lit les caractères s'il y en a de disponibles
   if(client.available()) {
     carlu = client.read();
@@ -76,7 +86,7 @@ void loop()
   // enregistre l'état de la connexion (ouvert ou fermé)
   etaitConnecte = client.connected();
 }
-
+/**********************Envoie des Donnée au Site WEB**********************************/
 void requete() {
   // On connecte notre Arduino sur "perdu.com" et le port 80 (defaut pour l'http)
   char erreur = client.connect(serveur, 80);
@@ -84,9 +94,6 @@ void requete() {
   if(erreur == 1) {
       // Pas d'erreur ? on continue !
       Serial.println("Connexion OK, envoi en cours...");
-    h = (int) dht.readHumidity();
-    t = (int) dht.readTemperature();
- data = String("temp1=") + t + "&hum1=" + h + "&co21=" + -1;
  Serial.print("Data:");
  Serial.println(data);
       // On construit l'en-tête de la requête
@@ -122,4 +129,31 @@ void requete() {
         break;
     }
   }
+}
+/**********************Lecture du Capteurs CO2**********************************/
+float MGRead(int mg_pin) {
+int i;
+float v = 0;
+for
+(i=0;i<READ_SAMPLE_TIMES;i++){
+v += analogRead(mg_pin);
+delay(READ_SAMPLE_INTERVAL);
+}
+v=(v/READ_SAMPLE_TIMES)*5/1024;
+return
+v;
+}
+int  MGGetPercentage(float volts, float *pcurve) {
+volts = volts / DC_GAIN;
+if
+(volts > ZERO_POINT_VOLTAGE || volts < MAX_POINT_VOLTAGE ) {
+return
+-1;
+}
+else
+{
+return
+pow(10, (volts - pcurve[1]) / pcurve[2] + pcurve[0]);
+volts = 0;
+}
 }
